9d01ac5c93272520178f73afe3169bf6699a2d07
mischa <mischa.ahi@gmx.de>
2016-02-15 17:02:46 +0100

    class pokemon, and class bisasam started

---
+++ PokeLike2/PokeLike2/Bisasam.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokeLike2
+{
+    class Bisasam : Pokemon
+    {
+        private Color color = Color.White;
+
+        public Bisasam(Vector2 position) : base(position, "Bisasam")
+        {
+            Name = "Bisasam";
+            Index = 001;
+            Health = 20;
+            Defense = 5;
+            AttackPower = 5;
+            Init = 5;
+            Element = "plant";
+            Lvl = 1;
+            Xp = 0;
+
+            collider.OnCollision += OnCollision;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Sprite, Position * Sprite.Width, color);
+        }
+
+        private void OnCollision()
+        {
+            //color = Color.Transparent;
+            //FightManager.Fight(this);
+        }
+
+        private void LvlUp()
+        {
+            double neededXp = 100 * Math.Pow((double)1.1f, (double)Lvl);
+            if (Xp > neededXp)
+            { 
+                Xp -= neededXp;
+                Lvl++;
+            }
+        }
+    }
+}
---
+++ PokeLike2/PokeLike2/Pokemon.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokeLike2
+{
+    abstract class Pokemon : GameObject
+    {
+        public int Index;
+        public int Health;
+        public int Defense;
+        public int AttackPower;
+        public int Lvl;
+        public double Xp;
+        public string Element;
+        public int Init;
+
+        public BoxCollider collider;
+
+        public Vector2 Position { get; private set; }
+        public Texture2D Sprite { get; private set; }
+
+        public Pokemon(Vector2 position, string texture)
+        {
+            this.Position = position;
+            this.Sprite = GameManager.Content.Load<Texture2D>("texture");
+
+            GameManager.AddGameObject(this);
+
+            collider = new BoxCollider((int)position.X, (int)position.Y, 1, 1);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+    }
+}
